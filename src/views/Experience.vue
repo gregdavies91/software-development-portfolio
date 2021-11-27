18c7015f27771d8ba2bcd92588dcb9d8a1a7864c
gregdavies91 <64627861+gregdavies91@users.noreply.github.com>
2021-11-27 14:34:58 +0800

    Unity working without component but in the experience.vue

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -22,15 +22,16 @@
     </ion-content>
 
   </ion-page>
-<!--    <canvas id="unity-canvas" width=900 height=600 style="width: 900px; height: 600px; background: #808080">
-        </canvas> -->
+   <canvas id="unity-canvas" style="display: block;
+  position: absolute; width: 100%; height: 100%; background: #808080">
+        </canvas>
         
-        <Unity>
-        </Unity>
+        <!-- <Unity>
+        </Unity> -->
 </template>
 
-<script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<!-- <script lang="ts"> -->
+<!-- import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import Unity from '../components/Unity.vue';
 
@@ -44,41 +45,41 @@ export default defineComponent({
     IonToolbar,
     Unity
   }
+}); -->
+<script setup>
+import { onMounted } from 'vue';
+
+const sendMessage = (object, method, param) => {
+  window.gameInstance.SendMessage(object, method, param);
+};
+
+onMounted(() => {
+  const file = 'BUILD_FILE';
+  const script = document.createElement('script');
+  script.onload = () => {
+    /*global createUnityInstance, a*/
+    createUnityInstance(document.querySelector('#unity-canvas'), {
+      dataUrl: `Build/${file}.data`,
+      frameworkUrl: `Build/${file}.framework.js`,
+      codeUrl: `Build/${file}.wasm`,
+      streamingAssetsUrl: 'StreamingAssets',
+      companyName: 'YOUR_COMPANY_NAME',
+      productName: 'YOUR_PRODUCT_NAME',
+      productVersion: 'YOUR_VERSION_NUMBER',
+      // matchWebGLToCanvasSize: false,
+      // Uncomment above to separately control WebGL canvas render size and DOM element size.
+      // devicePixelRatio: 1,
+      // Uncomment above to override low DPI rendering on high DPI displays.
+    }).then((unityInstance) => {
+      // setting this allows the usage of "window.gameInstance" in jslib plugins inside Unity
+      // it also sets up a simple shortcut we can use to provide a path into Unity from vue
+      window.gameInstance = unityInstance;
+    });
+  };
+  script.async = true;
+  script.src = `Build/${file}.loader.js`;
+  document.head.appendChild(script);
 });
-
-// import { onMounted } from 'vue';
-
-// const sendMessage = (object, method, param) => {
-//   window.gameInstance.SendMessage(object, method, param);
-// };
-
-// onMounted(() => {
-//   const file = 'BUILD_FILE';
-//   const script = document.createElement('script');
-//   script.onload = () => {
-//     /*global createUnityInstance, a*/
-//     createUnityInstance(document.querySelector('#unity-canvas'), {
-//       dataUrl: `Build/${file}.data`,
-//       frameworkUrl: `Build/${file}.framework.js`,
-//       codeUrl: `Build/${file}.wasm`,
-//       streamingAssetsUrl: 'StreamingAssets',
-//       companyName: 'YOUR_COMPANY_NAME',
-//       productName: 'YOUR_PRODUCT_NAME',
-//       productVersion: 'YOUR_VERSION_NUMBER',
-//       // matchWebGLToCanvasSize: false,
-//       // Uncomment above to separately control WebGL canvas render size and DOM element size.
-//       // devicePixelRatio: 1,
-//       // Uncomment above to override low DPI rendering on high DPI displays.
-//     }).then((unityInstance) => {
-//       // setting this allows the usage of "window.gameInstance" in jslib plugins inside Unity
-//       // it also sets up a simple shortcut we can use to provide a path into Unity from vue
-//       window.gameInstance = unityInstance;
-//     });
-//   };
-//   script.async = true;
-//   script.src = `Build/${file}.loader.js`;
-//   document.head.appendChild(script);
-// });
 
 </script>
 
