98a491c5c65f367f6a2814c5c61210579a7ba5e4
gregdavies91 <64627861+gregdavies91@users.noreply.github.com>
2021-12-01 10:20:56 +0800

    adding capacitor ios

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fun.simplistic.test',
+  appName: 'Greg Davies Portfolio',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
