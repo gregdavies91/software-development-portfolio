450a4f391933fb487611f29d59798fc68f2f9802
gregdavies91 <64627861+gregdavies91@users.noreply.github.com>
2021-12-03 00:30:37 +0800

    Added Ion Slides

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,108 +1,56 @@
 <template>
   <ion-page>
 
-    <ion-header>
-      <ion-toolbar style="--background: transparent">
+      <ion-header>
+        <ion-toolbar style="--background: transparent">
 
-<!--         This button has to be on all views, can it be in a component? -->
-        <ion-button fill="clear" background="transparent" slot='start'>
-          <ion-menu-button color="dark"></ion-menu-button>
-        </ion-button>
-        
-      </ion-toolbar>
-    </ion-header>
-    
+          <ion-button fill="clear" background="transparent" slot='start'>
+            <ion-menu-button color="dark"></ion-menu-button>
+          </ion-button>
+          
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-slides pager="true" :options="slideOpts">
+        <ion-slide>
+          <img src='../assets/images/landscape.png'>
+        </ion-slide>
+        <ion-slide>
+          <h1>Slide 2</h1>
+        </ion-slide>
+        <ion-slide>
+          <h1>Slide 3</h1>
+        </ion-slide>
+      </ion-slides>
 
   </ion-page>
-        
-        <canvas id="unity-canvas" style="display: block; position: absolute; width: 100%; height: 100%; background: #000000">
-        </canvas>
-        
-        <!-- <Unity>
-        </Unity> -->
+      
+  
 </template>
 
-<!-- <script lang="ts"> -->
-<!-- import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<style>
+  #experience {
+    display:  flex;
+    position: center;
+  }
+</style>
+
+<script>
+import { IonHeader, IonPage, IonToolbar, IonSlides, IonSlide } from '@ionic/vue';
 import { defineComponent } from 'vue';
-import Unity from '../components/Unity.vue';
 
 export default defineComponent({
-  name: 'Experience',
-  components: {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar,
-    Unity
+  name: "Experience",
+  components: { IonHeader, IonPage, IonToolbar, IonSlides, IonSlide },
+  setup() {
+    // Optional parameters to pass to the swiper instance. See http://idangero.us/swiper/api/ for valid options.
+    const slideOpts = {
+      initialSlide: 1,
+      speed: 400
+    };
+
+    return { slideOpts }
   }
-}); -->
-<script setup>
-import { onMounted } from 'vue';
-
-const sendMessage = (object, method, param) => {
-  window.gameInstance.SendMessage(object, method, param);
-};
-
-onMounted(() => {
-
-  const file = 'Experience2';
-
-  const script = document.createElement('script');
-  script.onload = () => {
-    /*global createUnityInstance, a*/
-    createUnityInstance(document.querySelector('#unity-canvas'), {
-      dataUrl: `Build/Experience2.data.br`,
-      frameworkUrl: `Build/Experience2.framework.js.br`,
-      codeUrl: `Build/Experience2.wasm.br`,
-      streamingAssetsUrl: 'StreamingAssets',
-      companyName: 'Leonids',
-      productName: "Experience",
-      productVersion: '0.2',
-      // matchWebGLToCanvasSize: false,
-      // Uncomment above to separately control WebGL canvas render size and DOM element size.
-      // devicePixelRatio: 1,
-      // Uncomment above to override low DPI rendering on high DPI displays.
-    }).then((unityInstance) => {
-      // setting this allows the usage of "window.gameInstance" in jslib plugins inside Unity
-      // it also sets up a simple shortcut we can use to provide a path into Unity from vue
-      window.gameInstance = unityInstance;
-    });
-  };
-  script.async = true;
-  script.src = `Build/Experience2.loader.js`;
-  document.head.appendChild(script);
 });
-
 </script>
 
-<style scoped>
-/*#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}*/
-</style>
