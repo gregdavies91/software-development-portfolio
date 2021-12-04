b71cb55484a48cfdbd5a8b2b7e2b1365ca8f7c5b
gregdavies91 <64627861+gregdavies91@users.noreply.github.com>
2021-12-04 15:33:29 +0800

    Deleted dynamic scroller and went with Grid
    fixed grid a bit on mobile to make it more responsive using media query inline.

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -12,43 +12,11 @@
       </ion-header>
 
     <ion-content>
-      <ion-list>
-        <DynamicScroller
-          class="scroller"
-          :items="list"
-          :item-size="56"
-        >
-          <template #default="{ item }">
-            <ion-item>
-              <ion-avatar slot="start">
-                <img src="../assets/images/landscape.png" />
-              </ion-avatar>
-              <ion-label>{{ item }}</ion-label>
-            </ion-item>
-          </template>
-        </DynamicScroller>
-      </ion-list>
-    </ion-content>
-      <!-- <ion-grid style="--ion-grid-width-xs:              100%;
---ion-grid-width-sm:              540px;
---ion-grid-width-md:              720px;
---ion-grid-width-lg:              960px;
---ion-grid-width-xl:              1140px; 
---ion-grid-padding:               5px;
-
---ion-grid-padding-xs:            5px;
---ion-grid-padding-sm:            5px;
---ion-grid-padding-md:            5px;
---ion-grid-padding-lg:            5px;
---ion-grid-padding-xl:            5px;
---ion-grid-column-padding:        5px;
-
---ion-grid-column-padding-xs:     5px;
---ion-grid-column-padding-sm:     5px;
---ion-grid-column-padding-md:     5px;
---ion-grid-column-padding-lg:     5px;
---ion-grid-column-padding-xl:     5px;">
-        <ion-row class="ion-justify-content-center">
+      
+    
+      <div class="container" id="container">
+      <ion-grid>
+        <ion-row>
           <ion-col size="12" size-sm>
               <img src="../assets/images/landscape.png">
           </ion-col>
@@ -62,42 +30,36 @@
             <img src="../assets/images/landscape.png">
           </ion-col>
         </ion-row>
-      </ion-grid> -->
-    
+      </ion-grid>
+    </div>
+    </ion-content>
 
   </ion-page>
       
   
 </template>
 
-<style>
+<style scoped>
+
+
+@media (max-width: 600px) {
   #container {
   text-align: center;
   
   position: absolute;
   left: 0;
   right: 0;
-  top: 50%;
+  top: 0;
   transform: translateY(-50%);
-}
+} 
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
+  ion-grid {
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
+  position: fixed;
   
-  color: #8c8c8c;
-  
-  margin: 0;
+  }
 }
 
-#container a {
-  text-decoration: none;
-}
 </style>
 
 <script lang="ts">
@@ -106,12 +68,9 @@ import {
     IonHeader, 
     IonPage, 
     IonToolbar, 
-    IonAvatar,
-    IonContent,
-    IonItem,
-    IonLabel 
+    IonContent
 } from '@ionic/vue';
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: "Experience",
@@ -119,16 +78,10 @@ export default defineComponent({
     IonHeader, 
     IonPage, 
     IonToolbar, 
-    IonAvatar,
-    IonContent,
-    IonItem,
-    IonLabel 
-  },
-setup() {
-      const list = ref([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    IonContent
+  }
+});
 
-      return { list }
-    }
-  });
+      
 </script>
 
