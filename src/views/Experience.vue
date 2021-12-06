5c1401454e42560010954dd68bfaf8eb4ef87bb1
gregdavies91 <64627861+gregdavies91@users.noreply.github.com>
2021-12-06 23:09:21 +0800

    Added transitions to opacity and text when hovering over divs
    Also added in some ideas for copywriting content

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -19,20 +19,23 @@
         <ion-row>
           <ion-col size="12" size-sm>
             <div class="trigger">
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/gregdavies91"><img src="../assets/images/landscape.png"></a>
-            </div>
-            <div class="hidden">
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/gregdavies91"><img src="../assets/images/landscape.png" style="opacity: 50%;"></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/gregdavies91"><img src="../assets/images/landscape.png"><p>Face To The Light Ltd.</p></a>
             </div>
           </ion-col>
           <ion-col size="12" size-sm>
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/gregdavies91"><img src="../assets/images/landscape.png"></a>
+            <div class="trigger">
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/gregdavies91"><img src="../assets/images/landscape.png"><p>Weengs Ltd.</p></a>
+            </div>
           </ion-col>
           <ion-col size="12" size-sm>
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/gregdavies91"><img src="../assets/images/landscape.png"></a>
+            <div class="trigger">
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/gregdavies91"><img src="../assets/images/landscape.png"><p>Vallie Ltd.</p></a>
+            </div>
           </ion-col>
           <ion-col size="12" size-sm>
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/gregdavies91"><img src="../assets/images/landscape.png"></a>
+            <div class="trigger">
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/gregdavies91"><img src="../assets/images/landscape.png"><p>Unlimit Ltd.</p></a>
+            </div>
           </ion-col>
         </ion-row>
       </ion-grid>
@@ -47,8 +50,24 @@
 <style scoped>
 
 
+#container {
+  padding-left: 200px;
+  padding-right: 200px;
+}
+
+@media (max-width: 1750px) {
+  #container {
+    padding-left: 50px;
+  padding-right: 50px;
+  }
+}
+
 @media (max-width: 575px) {
   #container {
+  
+  padding-left: 0px;
+  padding-right: 0px;
+
   text-align: center;
   
   position: absolute;
@@ -65,11 +84,39 @@
   }
 }
 
-.hidden { 
-  display: none;
+.trigger p {
+
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  visibility: hidden;
 }
-.trigger:hover + .hidden { 
-  display: inline; 
+
+.trigger:hover p {
+  
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  visibility: visible;
+  transition-property: visibility;
+  transition-delay: 0.2s;
+  transition-duration: 1s;
+}
+
+.trigger { 
+  
+  
+}
+.trigger:hover { 
+
+  opacity: 50%;
+  transition-property: opacity;
+  transition-duration: 0.5s;
+  
 }
 
 </style>
