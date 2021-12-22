fbe5fb58246fb1832c3aa1ae227c3814030c07a3
gregdavies91 <64627861+gregdavies91@users.noreply.github.com>
2021-12-22 16:48:35 +0800

    making the grid responsive

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -18,58 +18,58 @@
       <ion-grid>
         <ion-row>
           <ion-col size="12" size-sm>
-            <div class="trigger">
-              <a target="_blank" rel="noopener noreferrer" href="https://facetothelight.com"><img src="../assets/images/facetothelight.png"></a>
+            <div class="triggerfttl">
+              <a class="triggerfttl" target="_blank" rel="noopener noreferrer" href="https://facetothelight.com"></a>
             </div>
-            <div class="text">
-              <p style="color: white;">Face To The Light Ltd. - Product Owner
+            <!-- <div class="text"> -->
+              <!-- <p style="color: white;">Face To The Light Ltd. - Product Owner
               <br>
               <br>
               Overseeing all aspects of landing page creation, from copywriting, to UX/UI, to constant Conversion Rate Optimisation through data-driven A/B tests.
-              </p>
-            </div>
+              </p> -->
+            <!-- </div> -->
           </ion-col>
           <ion-col size="12" size-sm>
-            <div class="trigger">
-              <a target="_blank" rel="noopener noreferrer" href="https://weengs.co.uk"><img src="../assets/images/weengs.png"></a>
+            <div class="triggerweengs">
+              <a class="triggerweengs" target="_blank" rel="noopener noreferrer" href="https://weengs.co.uk"></a>
             </div>
-            <div class="text">
+            <!-- <div class="text"> -->
               <!-- <p style="color: white;">Weengs Ltd. - Product Owner
               <br>
               <br>
               Planned and estimated 2-week sprints in a realistic yet time-efficient manner that allowed the teams to deliver 97% of the MVP according to the company roadmap.
-              </p> -->
-              <img src="../assets/images/weengstext.png">
-            </div>
+              </p>
+              <img src="../assets/images/weengstext.png" class="imgtext"> -->
+            <!-- </div> -->
           </ion-col>
         </ion-row>
         <ion-row>
           <ion-col size="12" size-sm>
-            <div class="trigger">
-              <a target="_blank" rel="noopener noreferrer" href="https://techcrunch.com/2015/11/16/vallie-takes-the-luxe-valet-parking-model-and-puts-it-in-london/"><img src="../assets/images/vallie.png"></a>
+            <div class="triggervallie">
+              <a class="triggervallie" target="_blank" rel="noopener noreferrer" href="https://techcrunch.com/2015/11/16/vallie-takes-the-luxe-valet-parking-model-and-puts-it-in-london/"></a>
             </div>
-            <div class="text">
-              <p style="color: white;">Vallie Ltd. - Ops Manager
+            <!-- <div class="text"> -->
+              <!-- <p style="color: white;">Vallie Ltd. - Ops Manager
               <br>
               <br>
               Demonstrated their ability to manage a team and achieve results under outstanding pressure.
-              </p>
+              </p> -->
             
-            </div>
+            <!-- </div> -->
           </ion-col>
           <ion-col size="12" size-sm>
-            <div class="trigger">
+            <div class="triggerunlimit">
               
-              <a target="_blank" rel="noopener noreferrer" href="https://unlimit.co.uk"><img src="../assets/images/unlimit.png"></a>
+              <a class="triggerunlimit" target="_blank" rel="noopener noreferrer" href="https://unlimit.co.uk"></a>
               
             </div>
-            <div class="text">
+            <!-- <div class="text">
             <p style="color: white;">Unlimit Ltd. - Director
             <br>
             <br>
             While holding this Senior post gained relevant marketing experience in an executive role as Director in partnership with the software engineer team.
             </p>
-          </div>
+          </div> -->
             
             
           </ion-col>
@@ -86,48 +86,8 @@
 <style scoped>
 
 #container {
-  padding-left: 600px;
-  padding-right: 600px;
-}
-
-
-@media (max-width: 1875px) {
-  #container {
-  
-  padding-left: 500px;
-  padding-right: 500px;
-
-  }
- 
-}
-
-@media (max-width: 1575px) {
-  #container {
-  
-  padding-left: 300px;
-  padding-right: 300px;
-
-  }
- 
-}
-
-@media (max-width: 1275px) {
-  #container {
-  
   padding-left: 100px;
   padding-right: 100px;
-
-  }
- 
-}
-@media (max-width: 875px) {
-  #container {
-  
-  padding-left: 35px;
-  padding-right: 35px;
-
-  }
- 
 }
 
 @media (max-width: 575px) {
@@ -153,43 +113,116 @@
   }
 }
 
-.trigger + .text {
+/*.trigger + .text {
 
-  display:  none;
-  margin: 0;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  visibility: hidden;
-
+  display: none;
+  
   
 }
 
 .trigger:hover + .text {
   
-  opacity:  100%;
   position: absolute;
-  visibility: visible;
-  transition-property: visibility;
-  transition-delay: 0.2s;
-  transition-duration: 1s;
-  display: flex;
+  display: inherit;
+
+  
+}*/
+
+.triggerfttl { 
+  
+  width: 100%;
+  height: auto;
+  transition-property: background-image;
+  transition-duration: 0.5s;
+  background-image: url('../assets/images/weengs.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  display: inline-block;
+  
+}
+.triggerfttl:hover { 
+  
+  width: 100%;
+  height: auto;
+  transition-property: background-image;
+  transition-duration: 0.5s;
+  background-image: url('../assets/images/weengstext.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  display: inline-block;
   
 }
 
-.trigger { 
+.triggerweengs { 
   
-  opacity: 100%;
-  transition-property: opacity;
+  width: 100%;
+  height: auto;
+  transition-property: background-image;
   transition-duration: 0.5s;
+  background-image: url('../assets/images/weengs.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  display: inline-block;
   
 }
-.trigger:hover { 
+.triggerweengs:hover { 
   
-  opacity: 20%;
-  transition-property: opacity;
+  width: 100%;
+  height: auto;
+  transition-property: background-image;
   transition-duration: 0.5s;
+  background-image: url('../assets/images/weengstext.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  display: inline-block;
+  
+}
+.triggervallie { 
+  
+  width: 100%;
+  height: auto;
+  transition-property: background-image;
+  transition-duration: 0.5s;
+  background-image: url('../assets/images/weengs.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  display: inline-block;
+  
+}
+.triggervallie:hover { 
+  
+  width: 100%;
+  height: auto;
+  transition-property: background-image;
+  transition-duration: 0.5s;
+  background-image: url('../assets/images/weengstext.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  display: inline-block;
+  
+}
+.triggerunlimit { 
+  
+  width: 100%;
+  height: auto;
+  transition-property: background-image;
+  transition-duration: 0.5s;
+  background-image: url('../assets/images/weengs.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  display: inline-block;
+  
+}
+.triggerunlimit:hover { 
+  
+  width: 100%;
+  height: auto;
+  transition-property: background-image;
+  transition-duration: 0.5s;
+  background-image: url('../assets/images/weengstext.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  display: inline-block;
   
 }
 
